4e8697c419adf0b4f3d29d5bb553055d9f9eadd3
chrisstruong <chrisstruong@gmail.com>
2023-01-03 16:37:31 -0500

    Create and set up Movies.js

--- frontend/src/components/Movies.js
+++ frontend/src/components/Movies.js
@@ -1,7 +1,52 @@
+import { useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
 function Movies(props) {
+const [movies, setMovies] = useState([])
+
+const BASE_URL = 'http://localhost:4000/movies'
+
+const getMovies = async () => {
+    try {
+        const response = await fetch(BASE_URL)
+        const allMovies = await response.json()
+        setMovies(allMovies)
+    } catch (err){
+        console.log(err)
+    }
+}
+
+useEffect(() => {
+    getMovies()
+}, [])
+
+const loaded = () => {
     return (
         <>
-        <h1>This is Movies page</h1>
+            <section className="movie-list">
+                {movies?.map((movie) =>{
+                    return(
+                        <Link key={movie._id} to={`/movie/${movie._id}`}>
+                            <div className="movie-card">
+                                <h1>movie name</h1>
+                                <img>image</img>
+                                <h3>movie title</h3>
+                            </div>
+                        </Link>
+                    )
+                })}
+            </section>
+        </>
+    )
+}
+
+const loading = () => (
+    <section className="movie-list">
+        <h1>Loading... <span>{" "} <img className='spinner' src="https://freesvg.org/img/1544764567.png"/></span></h1>
+    </section>
+)
+    return (
+        <>
+        {movies && movies.length ? loaded() :loading()}
         </>
     )
 }
