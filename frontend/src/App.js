d2b54ee999b81f2421a2e06d8d98aa785fb49420
chrisstruong <chrisstruong@gmail.com>
2023-01-07 19:52:33 -0500

    Add view all top-rated-movies. Time to style drop-down-list and top-rated-movies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 import Movies from './components/Movies';
 import MovieDetails from './components/MovieDetails';
 import ReviewDetails from './components/ReviewDetails';
+import AllMovies from './components/AllMovies'
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Movies />} />
+        <Route path="/viewAllTopRatedMovies" element ={<AllMovies />}/>
         <Route path="/review/:id" element={<MovieDetails/>} />
         <Route path="/review/edit/:id" element={<ReviewDetails/>} />
       </Routes>
